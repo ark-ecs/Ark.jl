b22fe0aeaf862eccdd58af67cc349063c2f7e51a
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-12-31 01:16:38 +0100

    Update util.jl

--- src/util.jl
+++ src/util.jl
@@ -57,7 +57,9 @@ end
 
 # TODO: improve the heuristic with something more robust, as of 1.12 though Julia doesn't
 # expose anything to set the flag more correctly
-const _DEBUG = contains(Base.active_project(), "tmp/jl_") ? "true" : @load_preference("DEBUG", default = "false")
+is_testing() = contains(Base.active_project(), "tmp/jl_")
+
+const _DEBUG = is_testing() ? "true" : @load_preference("DEBUG", default = "false")
 
 macro check(arg)
     _DEBUG == "true" ? esc(:(@assert $arg)) : nothing
