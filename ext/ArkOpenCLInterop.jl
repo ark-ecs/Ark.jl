8be69d5d90e4c3c57880271424c3ef4749092036
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2026-01-12 22:57:31 +0100

    Update ArkOpenCLInterop.jl

--- ext/ArkOpenCLInterop.jl
+++ ext/ArkOpenCLInterop.jl
@@ -6,9 +6,9 @@ using Ark, OpenCL
 function Ark.gpuvector_type(::Type{T}, ::Val{:OpenCL}) where T
 	memory_backend = cl.unified_memory_backend()
     if memory_backend === cl.USMBackend()
-        return CLArray{T, 1, cl.UnifiedSharedMemory}
+        return CLArray{T,1,cl.UnifiedSharedMemory}
     elseif memory_backend === cl.SVMBackend()
-        return CLArray{T, 1, cl.SharedVirtualMemory}
+        return CLArray{T,1,cl.SharedVirtualMemory}
     else
         throw(ArgumentError("OpenCL storage not supported since no unified memory back-end was identified"))
     end
