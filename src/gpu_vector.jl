69d095947ae9c6020078be1ab933cd682a5adbe0
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-01-18 02:59:33 +0100

    Update gpu_vector.jl

--- src/gpu_vector.jl
+++ src/gpu_vector.jl
@@ -91,6 +91,11 @@ function Base.copyto!(gv::GPUVector, doffs::Integer, src::AbstractVector, soffs:
     return gv
 end
 
+function Base.copyto!(gv::GPUVector, doffs::Integer, src::GPUVector, soffs::Integer, n::Integer)
+    copyto!(gv.mem, doffs, src.mem, soffs, n)
+    return gv
+end
+
 function Base.similar(gv::GPUVector{B,T,M}, ::Type{T}, size::Dims{1}) where {B,T,M}
     return GPUVector{B,T,M}(M(undef, size), size[1])
 end
