426a13012a1e2e378de615ecb6dbb76522068961
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-01-18 02:21:31 +0100

    Update ArkCUDAInterop.jl

--- ext/ArkCUDAInterop.jl
+++ ext/ArkCUDAInterop.jl
@@ -7,8 +7,4 @@ function Ark._gpuvector_type(::Type{T}, ::Val{:CUDA}) where T
     return CuVector{T,CUDA.UnifiedMemory}
 end
 
-if !hasmethod(CUDA.Adapt.adapt_structure, Tuple{Any,GPUVector})
-    CUDA.Adapt.adapt_structure(to, w::GPUVector) = CUDA.Adapt.adapt(to, w.mem)
-end
-
 end
