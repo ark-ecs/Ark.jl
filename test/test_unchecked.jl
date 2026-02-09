746f595ad3462a985aef3c0f55db474fb703fa14
tortar <adrianomeligrana1999@gmail.com>
2026-02-10 00:22:36 +0100

    tests

--- test/test_unchecked.jl
+++ test/test_unchecked.jl
@@ -1,4 +1,39 @@
 
+@testset "macro expansion @unchecked" begin
+    e = @macroexpand @unchecked (h2,) = get_components(world, e, (Health,))
+    @test e == :((h2,) = get_components(world, e, (Health,); _unchecked=true))
+
+    e = @macroexpand @unchecked begin
+        for e in entities
+            e_copy = copy_entity!(world, e)
+            (h,) = get_components(world, e_copy, (Health,))
+
+            e_copy_2 = Ark.copy_entity!(world, e; add=(Position(1.0, 2.0),))
+            h, p = Ark.get_components(world, e_copy_2, (Health, Position))
+
+            e_copy_2 = Mod.copy_entity!(world, e; add=(Position(1.0, 2.0),))
+            h, p = Mod.get_components(world, e_copy_2, (Health, Position))
+        end
+    end
+
+    @test Base.remove_linenums!(e) == Base.remove_linenums!(
+        quote
+            for e in entities
+                e_copy = copy_entity!(world, e; _unchecked=true)
+                (h,) = get_components(world, e_copy, (Health,); _unchecked=true)
+                e_copy_2 = Ark.copy_entity!(world, e; add=(Position(1.0, 2.0),), _unchecked=true)
+                (h, p) = Ark.get_components(world, e_copy_2, (Health, Position); _unchecked=true)
+                e_copy_2 = Mod.copy_entity!(world, e; add=(Position(1.0, 2.0),))
+                (h, p) = Mod.get_components(world, e_copy_2, (Health, Position))
+            end
+        end,
+    )
+
+    e = @macroexpand @unchecked get_components(world, e, (Health,), get_components(world, e, (Health,)))
+    @test e ==
+          :(get_components(world, e, (Health,), get_components(world, e, (Health,); _unchecked=true); _unchecked=true))
+end
+
 @testset "get_components unchecked" begin
     world = World(Health, Position, ChildOf)
 
@@ -51,7 +86,7 @@ end
     world = World(Health, Position, ChildOf)
 
     @unchecked begin
-        e = new_entity!(world, (Health(10),))
+        e = Ark.new_entity!(world, (Health(10),))
         remove_entity!(world, e)
         @test !is_alive(world, e)
     end
