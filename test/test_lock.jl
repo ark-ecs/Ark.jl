bb9683161fb34c0572fe572ebe5e131c9da6e862
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-02-14 20:51:26 +0100

    Fix lock_counter access in test_lock.jl

--- test/test_lock.jl
+++ test/test_lock.jl
@@ -4,15 +4,15 @@
 
     # Initially, nothing should be locked
     @test !_is_locked(lock)
-    @test lock.lock_counter[] == 0
+    @test lock.lock_counter == 0
 
     # Lock one time
     _lock(lock)
     @test _is_locked(lock)
-    @test lock.lock_counter[] == 1
+    @test lock.lock_counter == 1
 
     # Unlock one time
     _unlock(lock)
-    @test lock.lock_counter[] == 0
+    @test lock.lock_counter == 0
     @test !_is_locked(lock)
 end
