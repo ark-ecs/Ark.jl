8962ee58fca9de01622995563cbf9fd1d1d84d18
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-02-14 15:57:36 +0100

    Update lock.jl

--- src/lock.jl
+++ src/lock.jl
@@ -1,4 +1,3 @@
-
 mutable struct _Lock
     @atomic lock_counter::Int
 end
@@ -8,19 +7,19 @@ function _Lock()
 end
 
 function _lock(lock::_Lock)::Int
-    @check lock.lock_counter >= 0
+    @check @atomic :monotonic lock.lock_counter >= 0
     @atomic :monotonic lock.lock_counter += 1
     return
 end
 
 function _unlock(lock::_Lock)
-    @check lock.lock_counter > 0
+    @check @atomic :monotonic lock.lock_counter > 0
     @atomic :monotonic lock.lock_counter -= 1
     return
 end
 
 function _is_locked(lock::_Lock)::Bool
-    @check lock.lock_counter >= 0
+    @check @atomic :monotonic lock.lock_counter >= 0
     return (@atomic :monotonic lock.lock_counter) != 0
 end
 
