21c3f4ca08defbbeaa3b2f477b9a9e599655198a
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-02-14 16:15:56 +0100

    Update lock.jl

--- src/lock.jl
+++ src/lock.jl
@@ -1,24 +1,24 @@
-mutable struct _Lock
-    @atomic lock_counter::Int
+struct _Lock
+    lock_counter::Threads.Atomic{Int}
 end
 
 function _Lock()
-    _Lock(0)
+    _Lock(Threads.Atomic{Int}(0))
 end
 
 function _lock(lock::_Lock)::Int
-    @check (@atomic :monotonic lock.lock_counter) >= 0
-    @atomic :monotonic lock.lock_counter += 1
+    @check lock.lock_counter[] >= 0
+    Threads.atomic_add!(lock.lock_counter, 1)
 end
 
 function _unlock(lock::_Lock)
-    @check (@atomic :monotonic lock.lock_counter) > 0
-    @atomic :monotonic lock.lock_counter -= 1
+    @check lock.lock_counter[] > 0
+    Threads.atomic_sub!(lock.lock_counter, 1)
 end
 
 function _is_locked(lock::_Lock)::Bool
-    @check (@atomic :monotonic lock.lock_counter) >= 0
-    return (@atomic :monotonic lock.lock_counter) != 0
+    @check lock.lock_counter[] >= 0
+    return lock.lock_counter[] != 0
 end
 
 function _reset!(lock::_Lock)
