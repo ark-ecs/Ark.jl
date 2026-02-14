bd88301294e01d6d2aff8ab3d98b45bc693093c3
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-02-14 04:01:02 +0100

    Update lock.jl

--- src/lock.jl
+++ src/lock.jl
@@ -8,18 +8,18 @@ function _Lock()
 end
 
 function _lock(lock::_Lock)::Int
-    @check lock.lock_counter >= 0
+    @check lock.lock_counter[] >= 0
     Threads.atomic_add!(lock.lock_counter, 1)
 end
 
 function _unlock(lock::_Lock)
-    @check lock.lock_counter > 0
+    @check lock.lock_counter[] > 0
     Threads.atomic_sub!(lock.lock_counter, 1)
 end
 
 function _is_locked(lock::_Lock)::Bool
-    @check lock.lock_counter >= 0
-    return lock.lock_counter != 0
+    @check lock.lock_counter[] >= 0
+    return lock.lock_counter[] != 0
 end
 
 function _reset!(lock::_Lock)
