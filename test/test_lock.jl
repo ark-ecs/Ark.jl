4fce4e8d73c517055d99e8a34bec24b8394a9323
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-02-14 16:29:20 +0100

    Update test_lock.jl

--- test/test_lock.jl
+++ test/test_lock.jl
@@ -1,18 +1,18 @@
 
-@testset "_Lock functionality" begin
+@testset "_Lock basic functionality" begin
     lock = _Lock()
 
     # Initially, nothing should be locked
     @test !_is_locked(lock)
-    @test lock.lock_counter == 0
+    @test lock.lock_counter[] == 0
 
     # Lock one time
     _lock(lock)
     @test _is_locked(lock)
-    @test lock.lock_counter == 1
+    @test lock.lock_counter[] == 1
 
     # Unlock one time
     _unlock(lock)
-    @test lock.lock_counter == 0
+    @test lock.lock_counter[] == 0
     @test !_is_locked(lock)
 end
