cf7f6762c462d671fcf11a771970695a0356c56b
ameligrana <adrianomeligrana1999@gmail.com>
2026-02-15 23:36:52 +0100

    fix

--- src/lock.jl
+++ src/lock.jl
@@ -1,31 +1,25 @@
-mutable struct _Counter
-    @atomic counter::Int
-end
 
-struct _Lock
-    _lock_counter::_Counter
+mutable struct _Lock
+    @atomic _counter::Int
 end
 
 function _Lock()
-    _Lock(_Counter(0))
+    _Lock(0)
 end
 
 function _lock(lock::_Lock)::Int
-    counter = lock._lock_counter
-    @check (@atomic :monotonic counter.counter) >= 0
-    @atomic :monotonic counter.counter += 1
+    @check (@atomic :monotonic lock._counter) >= 0
+    @atomic :monotonic lock._counter += 1
 end
 
 function _unlock(lock::_Lock)
-    counter = lock._lock_counter
-    @check (@atomic :monotonic counter.counter) > 0
-    @atomic :monotonic counter.counter -= 1
+    @check (@atomic :monotonic lock._counter) > 0
+    @atomic :monotonic lock._counter -= 1
 end
 
 function _is_locked(lock::_Lock)::Bool
-    counter = lock._lock_counter
-    @check (@atomic :monotonic counter.counter) >= 0
-    return (@atomic :monotonic counter.counter) != 0
+    @check (@atomic :monotonic lock._counter) >= 0
+    return (@atomic :monotonic lock._counter) != 0
 end
 
 function _reset!(lock::_Lock)
--- test/test_lock.jl
+++ test/test_lock.jl
@@ -4,15 +4,15 @@
 
     # Initially, nothing should be locked
     @test !_is_locked(lock)
-    @test lock._lock_counter.counter == 0
+    @test lock._counter == 0
 
     # Lock one time
     _lock(lock)
     @test _is_locked(lock)
-    @test lock._lock_counter.counter == 1
+    @test lock._counter == 1
 
     # Unlock one time
     _unlock(lock)
-    @test lock._lock_counter.counter == 0
+    @test lock._counter == 0
     @test !_is_locked(lock)
 end
