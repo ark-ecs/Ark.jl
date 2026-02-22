e579a3e0e4c14c77247b82d1b3eb7a58e19af95c
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-02-22 16:33:11 +0100

    Refactor _EntityPool to use Memory

--- src/pool.jl
+++ src/pool.jl
@@ -1,43 +1,64 @@
-
 mutable struct _EntityPool
-    const entities::Vector{Entity}
+    entities::Memory{Entity}
+    len::Int
     next::Int
 end
 
 function _EntityPool(cap::UInt32)
-    v = [_new_entity(UInt32(0), typemax(UInt32))]
-    sizehint!(v, cap)
-
-    return _EntityPool(v, 0)
+    mem_cap = max(2, Int(cap))
+    mem = Memory{Entity}(undef, mem_cap)
+    
+    @inbounds mem[1] = _new_entity(UInt32(0), typemax(UInt32))
+    
+    return _EntityPool(mem, 1, 0)
 end
 
-function _get_entity(p::_EntityPool)::Entity
+@inline function _get_entity(p::_EntityPool)::Entity
     if p.next == 0
         return _get_new_entity(p)
     end
+    
     curr = p.next
-    temp = p.entities[curr]
+    @inbounds temp = p.entities[curr]
 
     p.next = temp._id
-    entity = _Entity(curr % UInt32, temp._gen)
-    p.entities[curr] = entity
+    
+    entity = _Entity(curr % UInt32, temp._gen) 
+    @inbounds p.entities[curr] = entity
 
     return entity
 end
 
-function _get_new_entity(p::_EntityPool)::Entity
-    e = _new_entity(length(p.entities) + 1, 0)
-    push!(p.entities, e)
+@inline function _get_new_entity(p::_EntityPool)::Entity
+    len = p.len + 1
+    
+    if len > length(p.entities)
+        old_cap = length(p.entities)
+
+        new_cap = clamp(overallocation(old_cap), old_cap+1, typemax(UInt32))
+
+        new_mem = Memory{Entity}(undef, new_cap)
+        unsafe_copyto!(new_mem, 1, p.entities, 1, p.len)
+        p.entities = new_mem
+    end
+    
+    p.len = len
+    e = _new_entity(len % UInt32, UInt32(0))
+    @inbounds p.entities[len] = e
+    
     return e
 end
 
-function _recycle(p::_EntityPool, e::Entity)
+@inline function _recycle(p::_EntityPool, e::Entity)
     if e._id < 2
         throw(ArgumentError("can't recycle the reserved zero entity"))
     end
+    
     temp = p.next
     p.next = e._id
-    p.entities[e._id] = _new_entity(temp % UInt32, e._gen + UInt32(1))
+    
+    @inbounds p.entities[e._id] = _new_entity(temp % UInt32, e._gen + UInt32(1))
+    
     return nothing
 end
 
@@ -46,6 +67,21 @@ function _is_alive(p::_EntityPool, e::Entity)::Bool
 end
 
 function _reset!(p::_EntityPool)
-    resize!(p.entities, 1)
+    p.len = 1
     p.next = 0
+    return nothing
+end
+
+# Copied from base/array.jl because this is not a public function
+# https://github.com/JuliaLang/julia/blob/v1.11.6/base/array.jl#L1042-L1056
+function overallocation(maxsize)
+    maxsize < 8 && return 8;
+    # compute maxsize = maxsize + 4*maxsize^(7/8) + maxsize/8
+    # for small n, we grow faster than O(n)
+    # for large n, we grow at O(n/8)
+    # and as we reach O(memory) for memory>>1MB,
+    # this means we end by adding about 10% of memory each time
+    exp2 = sizeof(maxsize) * 8 - Core.Intrinsics.ctlz_int(maxsize)
+    maxsize += (1 << div(exp2 * 7, 8)) * 4 + div(maxsize, 8)
+    return maxsize
 end
