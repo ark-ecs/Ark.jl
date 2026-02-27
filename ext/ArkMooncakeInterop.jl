e1000164fd17457cd9c28442f15b7d1cc833a211
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-02-27 12:51:25 +0100

    Remove mooncake_overlay sizehint! method

--- ext/ArkMooncakeInterop.jl
+++ ext/ArkMooncakeInterop.jl
@@ -7,6 +7,4 @@ Mooncake.tangent_type(::Type{<:Ark._GraphNode}) = Mooncake.NoTangent
 Mooncake.tangent_type(::Type{<:Ark._VecMap}) = Mooncake.NoTangent
 Mooncake.tangent_type(::Type{<:Ark._EventManager}) = Mooncake.NoTangent
 
-Mooncake.@mooncake_overlay Base.sizehint!(A::AbstractVector, n::Integer) = A
-
 end
