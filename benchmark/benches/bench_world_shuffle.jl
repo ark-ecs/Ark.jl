e2d8b59b84a72552fb1e96eb887e6883cbb0f51c
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-04-30 03:48:13 +0200

    Update bench_world_shuffle.jl

--- benchmark/benches/bench_world_shuffle.jl
+++ benchmark/benches/bench_world_shuffle.jl
@@ -11,14 +11,14 @@ function setup_world_shuffle(n_entities::Int)
         new_entity!(world, (
                 CompN{1}(i, i), CompN{2}(i, i), CompN{3}(i, i), CompN{4}(i, i), CompN{5}(i, i),
                 CompN{6}(i, i), CompN{7}(i, i), CompN{8}(i, i), CompN{9}(i, i), CompN{10}(i, i),
-            )
+            ),
         )
     end
 
     f = Filter(world, (
             CompN{1}, CompN{2}, CompN{3}, CompN{4}, CompN{5},
             CompN{6}, CompN{7}, CompN{8}, CompN{9}, CompN{10},
-        )
+        ),
     )
     rng = Xoshiro(42)
     shuffle_entities!(rng, f)
@@ -32,7 +32,7 @@ end
 
 function benchmark_world_sort(args)
     _, f = args
-    sort_entities!(f; by=e->f._world[e][CompN{1}].x)
+    sort_entities!(f; by=e -> f._world[e][CompN{1}].x, alg=Base.Sort.QuickSort)
 end
 
 for n in (100, 10_000)
@@ -42,5 +42,5 @@ end
 
 for n in (100, 10_000)
     SUITE["benchmark_world_sort n=$(n)"] =
-        @be setup_world_shuffle($n) benchmark_world_sort(_) evals=1 seconds = SECONDS
+        @be setup_world_shuffle($n) benchmark_world_sort(_) evals = 1 seconds = SECONDS
 end
