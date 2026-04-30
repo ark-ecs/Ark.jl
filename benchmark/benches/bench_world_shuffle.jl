b12e1f6d8e936b21ada8047d81b6f2811d2357e5
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-04-30 02:03:23 +0200

    Use more components in shuffle entities

--- benchmark/benches/bench_world_shuffle.jl
+++ benchmark/benches/bench_world_shuffle.jl
@@ -2,11 +2,26 @@
 using Random
 
 function setup_world_shuffle(n_entities::Int)
-    world = World(Position, Velocity)
-    for i in 1:n_entities
-        new_entity!(world, (Position(i, i), Velocity(i, i)))
+    world = World(
+        CompN{1}, CompN{2}, CompN{3}, CompN{4}, CompN{5},
+        CompN{6}, CompN{7}, CompN{8}, CompN{9}, CompN{10}
+    )
+
+    entities = Vector{Entity}()
+
+    for _ in 1:n_entities
+        new_entity!(world, (
+            CompN{1}(0, 0), CompN{2}(0, 0), CompN{3}(0, 0), CompN{4}(0, 0), CompN{5}(0, 0),
+            CompN{6}(0, 0), CompN{7}(0, 0), CompN{8}(0, 0), CompN{9}(0, 0), CompN{10}(0, 0),
+        ))
     end
-    f = Filter(world, (Position, Velocity))
+
+    return (entities, world)
+end
+    f = Filter(world, (
+        CompN{1}, CompN{2}, CompN{3}, CompN{4}, CompN{5},
+        CompN{6}, CompN{7}, CompN{8}, CompN{9}, CompN{10}
+    ))
     rng = Xoshiro(42)
     return (rng, f)
 end
