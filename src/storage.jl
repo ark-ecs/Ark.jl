afba460479eeb7ac14e5b7a19714af7524f27d7c
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-05-02 00:05:13 +0200

    Update storage.jl

--- src/storage.jl
+++ src/storage.jl
@@ -328,7 +328,7 @@ end
 
     return quote
         @inbounds col = s.data[table]
-        comps = getfield(col, :_components)
+        $(A <: _AbstractStructArray ? :(comps = getfield(col, :_components) : (:(nothing))))
 
         $(tmp_exprs...)
 
