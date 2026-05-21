fab22687f17ac10d57762da0c7545a98c9be2a1c
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-05-21 21:46:26 +0200

    Update util.jl

--- src/util.jl
+++ src/util.jl
@@ -149,6 +149,8 @@ function _has_relations(declared_relations::Type{<:Tuple})
     return !isempty(declared_relations.parameters)
 end
 
+_relation_types_and_targets(::Tuple{}) = tuple()
+
 @inline @generated function _relation_types_and_targets(
     relations::Tuple{Vararg{Any,N}},
 ) where {N<:Integer}
@@ -164,6 +166,8 @@ end
         return $rel_types, $targets
     end
 end
+
+_valtuple(::Tuple{}) = tuple()
 
 @inline @generated function _valtuple(t::Tuple{Vararg{Any,N}}) where {N<:Integer}
     return Expr(:tuple, (:(Val(getfield(t, $i))) for i in 1:N)...)
