635ddde7429b59aa26c037d80c0addb5c4cef0db
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-05-29 02:21:07 +0200

    Update bench_query_posvel_nested.jl

--- benchmark/benches/bench_query_posvel_nested.jl
+++ benchmark/benches/bench_query_posvel_nested.jl
@@ -1,55 +1,60 @@
+function setup_query_create_relation_world(n)
+    world = World(Position, Velocity, Relation{ChildOf})
+    parents = Vector{Entity}(undef, n)
 
-function setup_query_posvel_nested(n_entities::Int)
-    n_parents = div(n_entities, 100)
-
-    world = World(
-        Position,
-        Velocity,
-        Relation{ChildOf};
-    )
-    parents = Vector{Entity}(undef, n_parents)
-
-    for i in 1:n_parents
+    for i in 1:n
         parent = new_entity!(world, (Position(i, i * 2), Velocity(1, 1)))
-        parents[i] = parent
-        new_entities!(
-            world,
-            100,
-            (Position(0, 0), Velocity(1, 1), ChildOf() => parent),
-        )
+        @inbounds parents[i] = parent
+        new_entity!(world, (Position(0, 0), Velocity(1, 1), ChildOf() => parent))
     end
 
-    return world, parents, sum
+    return world, parents
 end
 
-function benchmark_query_posvel_nested(args, n)
-    world, parents, _ = args
-    sum = 0
-    parent_index = 1
+function setup_query_create_relation(n)
+    world, parents = setup_query_create_relation_world(n)
+    benchmark_query_create_relation((world, parents), n)
+    return world, parents
+end
 
-    for (_, parent_pos_column, parent_vel_column) in Query(world, (Position, Velocity); without=(ChildOf,))
-        for parent_i in eachindex(parent_pos_column)
-            @inbounds parent = parents[parent_index]
-            for (_, child_pos_column, child_vel_column, _) in Query(world, (Position, Velocity, ChildOf => parent))
-                for child_i in eachindex(child_pos_column)
-                    @inbounds pos = child_pos_column[child_i]
-                    @inbounds vel = child_vel_column[child_i]
-                    @inbounds child_pos_column[child_i] = Position(pos.x + vel.dx, pos.y + vel.dy)
-                    sum += 1
-                end
-            end
+function benchmark_query_create_relation(args, n)
+    world, parents = args
 
-            @inbounds pos = parent_pos_column[parent_i]
-            @inbounds vel = parent_vel_column[parent_i]
-            @inbounds parent_pos_column[parent_i] = Position(pos.x + vel.dx, pos.y + vel.dy)
-            parent_index += 1
-        end
+    for i in 1:n
+        @inbounds parent = parents[i]
+        query = Query(world, (Position, Velocity, ChildOf => parent))
+        close!(query)
     end
 
-    return sum
+    return world
 end
 
-for n in (100, 10_000)
-    SUITE["benchmark_query_posvel_nested n=$(n)"] =
-        @be setup_query_posvel_nested($n) benchmark_query_posvel_nested(_, $n) evals = 1 seconds = SECONDS
+SUITE["benchmark_query_create_relation n=1000"] =
+    @be setup_query_create_relation($1000) benchmark_query_create_relation(_, $1000) seconds = SECONDS
+
+function setup_query_create_relation_filter(n)
+    world, parents = setup_query_create_relation_world(n)
+
+    first_filter = Filter(world, (Position, Velocity, ChildOf => parents[1]))
+    filters = Vector{typeof(first_filter)}(undef, n)
+    filters[1] = first_filter
+    for i in 2:n
+        @inbounds filters[i] = Filter(world, (Position, Velocity, ChildOf => parents[i]))
+    end
+
+    benchmark_query_create_relation_filter(filters, n)
+    return filters
 end
+
+function benchmark_query_create_relation_filter(filters, n)
+    for i in 1:n
+        @inbounds filter = filters[i]
+        query = Query(filter)
+        close!(query)
+    end
+
+    return filters
+end
+
+SUITE["benchmark_query_create_relation_filter n=1000"] =
+    @be setup_query_create_relation_filter($1000) benchmark_query_create_relation_filter(_, $1000) seconds = SECONDS
