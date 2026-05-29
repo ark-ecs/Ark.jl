f9ddb1685b40cf3123e7cbbdd84d2391db353acf
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-05-29 22:09:21 +0200

    Update query.jl

--- src/query.jl
+++ src/query.jl
@@ -231,7 +231,7 @@ end
 
 @inline function Base.first(q::Q) where {Q<:Query}
     x = iterate(q)
-    x === nothing && throw(ArgumentError("collection must be non-empty"))
+    x === nothing && throw(BoundsError("collection must be non-empty"))
     return x[1]
 end
 
