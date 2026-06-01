ac51ee8832be199c01ae10c5833355fe49b29c0f
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-06-01 17:43:16 +0200

    Implement in function for _Linear_Map_Keys

--- src/linear_map.jl
+++ src/linear_map.jl
@@ -276,3 +276,9 @@ Base.IteratorSize(::Type{<:_Linear_Map_Values}) = Base.HasLength()
 Base.eltype(::Type{<:_Linear_Map{K,V}}) where {K,V} = Pair{K,V}
 Base.eltype(::Type{<:_Linear_Map_Keys{K,V}}) where {K,V} = K
 Base.eltype(::Type{<:_Linear_Map_Values{K,V}}) where {K,V} = V
+
+function Base.in(key, dk::_Linear_Map_Keys)
+    d = dk.d
+    @_get_value_loop(true)
+    return false
+end
