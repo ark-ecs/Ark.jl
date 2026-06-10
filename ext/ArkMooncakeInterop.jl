31ef40f3866f91c936204a843c5611de5c3f8215
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-06-10 19:22:43 +0200

    Update ArkMooncakeInterop.jl

--- ext/ArkMooncakeInterop.jl
+++ ext/ArkMooncakeInterop.jl
@@ -7,14 +7,6 @@ Mooncake.tangent_type(::Type{<:Ark._GraphNode}) = Mooncake.NoTangent
 
 Mooncake.tangent_type(::Type{<:Ark._VecMap}) = Mooncake.NoTangent
 
-Mooncake.@mooncake_overlay function _observer_show_strings(
-    comps::Ark._Mask{M},
-    with::Ark._Mask{M},
-    without::Ark._Mask{M},
-    is_exclusive::Bool,
-    ::Type{W},
-) where {M,W<:Ark._AbstractWorld}
-    return "", "", ""
-end
+Mooncake.tangent_type(::Type{<:Ark._EventManager}) = Mooncake.NoTangent
 
 end
