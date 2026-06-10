9991d4cb88e807a0720ed9bc2471314eac43d07d
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-06-10 19:08:37 +0200

    Update ArkMooncakeInterop.jl

--- ext/ArkMooncakeInterop.jl
+++ ext/ArkMooncakeInterop.jl
@@ -4,7 +4,9 @@ module ArkMooncakeInterop
 using Ark, Mooncake, Mooncake.Random
 
 Mooncake.tangent_type(::Type{<:Ark._GraphNode}) = Mooncake.NoTangent
+
 Mooncake.tangent_type(::Type{<:Ark._VecMap}) = Mooncake.NoTangent
+
 Mooncake.@mooncake_overlay function _observer_show_strings(
     comps::_Mask{M},
     with::_Mask{M},
