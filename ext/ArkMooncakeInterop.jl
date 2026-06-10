ea7242d657b823e959fcd7bcfdcfe622dc6db791
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-06-10 19:50:59 +0200

    Update ArkMooncakeInterop.jl

--- ext/ArkMooncakeInterop.jl
+++ ext/ArkMooncakeInterop.jl
@@ -7,6 +7,13 @@ Mooncake.tangent_type(::Type{<:Ark._GraphNode}) = Mooncake.NoTangent
 
 Mooncake.tangent_type(::Type{<:Ark._VecMap}) = Mooncake.NoTangent
 
-Mooncake.tangent_type(::Type{<:Ark._EventManager}) = Mooncake.NoTangent
+Mooncake.@zero_adjoint Mooncake.DefaultCtx Tuple{
+    typeof(Ark._observer_show_strings),
+    Ark._Mask{M},
+    Ark._Mask{M},
+    Ark._Mask{M},
+    Bool,
+    Type{W},
+} where {M, W<:Ark._AbstractWorld}
 
 end
