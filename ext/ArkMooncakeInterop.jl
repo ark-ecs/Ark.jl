4721e8facfa6aa71ccbb79bf6c08d3c9ae9ff27a
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-06-10 20:17:43 +0200

    Update ArkMooncakeInterop.jl

--- ext/ArkMooncakeInterop.jl
+++ ext/ArkMooncakeInterop.jl
@@ -7,13 +7,22 @@ Mooncake.tangent_type(::Type{<:Ark._GraphNode}) = Mooncake.NoTangent
 
 Mooncake.tangent_type(::Type{<:Ark._VecMap}) = Mooncake.NoTangent
 
-Mooncake.@zero_adjoint Mooncake.DefaultCtx Tuple{
+Mooncake.@zero_derivative Mooncake.DefaultCtx Tuple{
     typeof(Ark._observer_show_strings),
     Ark._Mask{M},
     Ark._Mask{M},
     Ark._Mask{M},
     Bool,
     Type{W},
-} where {M,W<:Ark._AbstractWorld}
+} where {M,W<:Ark._AbstractWorld} Mooncake.ForwardMode
+
+Mooncake.@zero_derivative Mooncake.DefaultCtx Tuple{
+    typeof(Ark._observer_show_strings),
+    Ark._Mask{M},
+    Ark._Mask{M},
+    Ark._Mask{M},
+    Bool,
+    Type{W},
+} where {M,W<:Ark._AbstractWorld} Mooncake.ReverseMode
 
 end
